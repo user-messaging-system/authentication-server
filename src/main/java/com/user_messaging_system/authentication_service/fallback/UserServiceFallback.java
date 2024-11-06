8e2962191b0040f40af04c9cf006fc4bb46b73b2
burakyapici747 <burakyapici747@gmail.com>
2024-11-06 10:07:28 +0300

    feat(api-gateway): add client and fallback classes for user service communication

---
+++ src/main/java/com/user_messaging_system/authentication_service/fallback/UserServiceFallback.java
@@ -0,0 +1,21 @@
+package com.user_messaging_system.authentication_service.fallback;
+
+import com.user_messaging_system.authentication_service.api.response.FallbackResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class UserServiceFallback {
+    public ResponseEntity<FallbackResponse> getByEmail_Fallback(){
+        FallbackResponse fallbackResponse = new FallbackResponse(
+                "user-service",
+                "User Service is currently unavailable/ Bu mesaj getByEmail_Fallback tarafindan olusturuldu.",
+                String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(fallbackResponse);
+    }
+}
