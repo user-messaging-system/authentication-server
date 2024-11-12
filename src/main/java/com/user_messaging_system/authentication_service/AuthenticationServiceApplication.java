e4e4a3d6e459024f1dc73722eefaf623785f74cc
burak <burakyapici747@gmail.com>
2024-11-12 19:33:55 +0300

    fix: explicitly import JWTService in AuthenticationService and MessageService

--- src/main/java/com/user_messaging_system/authentication_service/AuthenticationServiceApplication.java
+++ src/main/java/com/user_messaging_system/authentication_service/AuthenticationServiceApplication.java
@@ -1,11 +1,14 @@
 package com.user_messaging_system.authentication_service;
 
+import com.user_messaging_system.core_library.service.JWTService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = {"com.user_messaging_system.core_library"})
+@SpringBootApplication
 @EnableFeignClients
+@Import(JWTService.class)
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
