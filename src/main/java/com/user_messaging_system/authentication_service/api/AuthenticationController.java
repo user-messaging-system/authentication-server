cf664c3e6c077d52e7187c465a03ecf75f5f5a26
burak <burakyapici747@gmail.com>
2024-11-12 19:30:49 +0300

    fix(AuthenticationController): add leading slash to API path

--- src/main/java/com/user_messaging_system/authentication_service/api/AuthenticationController.java
+++ src/main/java/com/user_messaging_system/authentication_service/api/AuthenticationController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/api/auth")
+@RequestMapping("/v1/api/auth")
 public class AuthenticationController {
 }
