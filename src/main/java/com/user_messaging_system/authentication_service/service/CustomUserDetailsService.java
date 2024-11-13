31bb6421c3047d2a9255955914265450e5f93376
burak <burakyapici747@gmail.com>
2024-11-14 00:05:18 +0300

    feat(authentication-service): use CustomUserDetails in UserDetailsService for improved user details management
    - added CustomUserDetails class to handle user details with custom fields like ID
    - updated CustomUserDetailsService to use CustomUserDetails instead of default User implementation
    - modified loadUserByUsername to map roles using SimpleGrantedAuthority and provide custom user details

--- src/main/java/com/user_messaging_system/authentication_service/service/CustomUserDetailsService.java
+++ src/main/java/com/user_messaging_system/authentication_service/service/CustomUserDetailsService.java
@@ -2,11 +2,13 @@ package com.user_messaging_system.authentication_service.service;
 
 import com.user_messaging_system.authentication_service.api.output.UserOutput;
 import com.user_messaging_system.authentication_service.client.UserClient;
+import com.user_messaging_system.core_library.exception.UserNotFoundException;
+import com.user_messaging_system.core_library.response.CustomUserDetails;
 import com.user_messaging_system.core_library.response.SuccessResponse;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -29,14 +31,19 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         ResponseEntity<SuccessResponse<UserOutput>> userOutputResponseEntity = userClient.getByEmail(username);
         UserOutput userOutput = checkUserOutput(userOutputResponseEntity);
-        return new User(userOutput.getEmail(), userOutput.getPassword(), getGrantedAuthority(userOutput.getRoles()));
+        return new CustomUserDetails(
+                userOutput.id(),
+                userOutput.email(),
+                userOutput.password(),
+                userOutput.roles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList())
+        );
     }
 
     private UserOutput checkUserOutput(ResponseEntity<SuccessResponse<UserOutput>> userOutputResponseEntity){
         if(Objects.nonNull(userOutputResponseEntity) && Objects.nonNull(userOutputResponseEntity.getBody())){
-            return (UserOutput)userOutputResponseEntity.getBody().getData();
+            return userOutputResponseEntity.getBody().getData();
         }
-        throw new UsernameNotFoundException("User not found");
+        throw new UserNotFoundException("User not found");
     }
 
     private List<GrantedAuthority> getGrantedAuthority(List<String> roles){
