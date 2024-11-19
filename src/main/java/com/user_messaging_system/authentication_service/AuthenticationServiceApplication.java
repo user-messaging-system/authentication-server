5f11aca512630aeed9beca969dc06c11c2080ec0
burak <burakyapici747@gmail.com>
2024-11-19 22:33:35 +0300

    feat(authentication-service): integrate JWTService and add Feign error decoder with full logging level

--- src/main/java/com/user_messaging_system/authentication_service/AuthenticationServiceApplication.java
+++ src/main/java/com/user_messaging_system/authentication_service/AuthenticationServiceApplication.java
@@ -1,18 +1,30 @@
 package com.user_messaging_system.authentication_service;
 
+import com.user_messaging_system.authentication_service.client.UserErrorDecoder;
 import com.user_messaging_system.core_library.service.JWTService;
+import feign.Logger;
+import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableFeignClients
 @Import(JWTService.class)
 public class AuthenticationServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
 	}
 
+    @Bean
+    public ErrorDecoder errorDecoder(){
+        return new UserErrorDecoder();
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
