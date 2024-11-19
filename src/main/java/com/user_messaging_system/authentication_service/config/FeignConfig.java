ab959e767b588a81c6d6021268ceac0faf2f67fb
burak <burakyapici747@gmail.com>
2024-11-19 22:37:46 +0300

    feat(authentication-service): add FeignConfig with custom UserErrorDecoder bean

---
+++ src/main/java/com/user_messaging_system/authentication_service/config/FeignConfig.java
@@ -0,0 +1,14 @@
+package com.user_messaging_system.authentication_service.config;
+
+import com.user_messaging_system.authentication_service.client.UserErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public UserErrorDecoder userErrorDecoder(){
+        return new UserErrorDecoder();
+    }
+}
