0d57913cb7e47ddce0fb1bfddeb24e7ab488baed
Burak <burakyapici747@gmail.com>
2024-11-24 21:25:54 +0300

    refactor: Added UserRegisterDTO and refactor

--- src/main/java/com/user_messaging_system/authentication_service/config/FeignConfig.java
+++ src/main/java/com/user_messaging_system/authentication_service/config/FeignConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
-
     @Bean
     public UserErrorDecoder userErrorDecoder(){
         return new UserErrorDecoder();
